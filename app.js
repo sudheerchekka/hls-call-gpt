0cc40e6a186dd3d83f6c491c65779f138814b49b
Sudheer Chekka <schekka@twilio.com>
2024-01-22 23:58:03 -0800

    using separate media streams for each participant and summarizing the conversation

--- app.js
+++ app.js
@@ -12,16 +12,46 @@ const { TextToSpeechService } = require("./services/tts-service");
 const app = express();
 ExpressWs(app);
 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+let confSid;
+
+let conversation="";
+let sampleConversation=process.env.SAMPLE_CONVO;
+
 const PORT = process.env.PORT || 3000;
 
-app.post("/incoming", (req, res) => {
+app.post("/incoming/patient", (req, res) => {
   res.status(200);
   res.type("text/xml");
   res.end(`
   <Response>
-    <Connect>
-      <Stream url="wss://${process.env.SERVER}/connection" />
-    </Connect>
+    <Start>
+      <Stream url="wss://${process.env.SERVER}/connection">
+        <Parameter name="participant" value ="patient"/>
+      </Stream>
+    </Start>
+    <Dial>
+      <Conference>Room 1234</Conference>
+    </Dial>
+  </Response>
+  `);
+});
+
+app.post("/incoming/clinician", (req, res) => {
+  res.status(200);
+  res.type("text/xml");
+  res.end(`
+  <Response>
+    <Start>
+      <Stream url="wss://${process.env.SERVER}/connection">
+        <Parameter name="participant" value ="clinician"/>
+      </Stream>
+    </Start>
+    <Dial>
+      <Conference>Room 1234</Conference>
+    </Dial>
   </Response>
   `);
 });
@@ -29,12 +59,14 @@ app.post("/incoming", (req, res) => {
 app.ws("/connection", (ws, req) => {
   ws.on("error", console.error);
   // Filled in from start message
-  let streamSid;
+  let patientStreamSid;
+  let clinicianStreamSid;
 
   const gptService = new GptService();
-  const streamService = new StreamService(ws);
-  const transcriptionService = new TranscriptionService();
-  const ttsService = new TextToSpeechService({});
+  //const streamService = new StreamService(ws);
+  const patient_transcriptionService = new TranscriptionService();
+  const clinician_transcriptionService = new TranscriptionService();
+  //const ttsService = new TextToSpeechService({});
   
   let marks = []
   let interactionCount = 0
@@ -43,44 +75,91 @@ app.ws("/connection", (ws, req) => {
   ws.on("message", function message(data) {
     const msg = JSON.parse(data);
     if (msg.event === "start") {
-      streamSid = msg.start.streamSid;
-      streamService.setStreamSid(streamSid);
-      console.log(`Twilio -> Starting Media Stream for ${streamSid}`.underline.red);
-      ttsService.generate({partialResponseIndex: null, partialResponse: "Hello! I understand you're looking for a pair of AirPods, is that correct?"}, 1);
+      user = msg.start.customParameters.participant;
+      if (user === 'patient'){
+        patientStreamSid = msg.start.streamSid;
+        console.log(`Starting patient Media Stream for ${patientStreamSid}`);
+      }
+      else if (user === 'clinician'){
+        clinicianStreamSid = msg.start.streamSid;
+        console.log(`Starting clinician Media Stream for ${clinicianStreamSid}`);
+      }
     } else if (msg.event === "media") {
-      transcriptionService.send(msg.media.payload);
+        if (msg.streamSid == patientStreamSid)
+          patient_transcriptionService.send(msg.media.payload);
+        else
+          clinician_transcriptionService.send(msg.media.payload);
     } else if (msg.event === "mark") {
       const label = msg.mark.name;
       console.log(`Twilio -> Audio completed mark (${msg.sequenceNumber}): ${label}`.red)
       marks = marks.filter(m => m !== msg.mark.name)
     } else if (msg.event === "stop") {
-      console.log(`Twilio -> Media stream ${streamSid} ended.`.underline.red)
+      if (msg.streamSid == patientStreamSid)
+        console.log(`Twilio -> Patient Media stream ${patientStreamSid} ended.`.underline.red)
+      else{
+        console.log(`Twilio -> Clinician Media stream ${clinicianStreamSid} ended.`.underline.red)
+
+     //TODO: close the conference when the clincian ends the call
+
+      //print the consolidated conversation
+        console.log ("==============================================");
+        console.log (conversation);
+        console.log ("==============================================");  
+
+        //call GPT service to summarize the sample conversation
+        //TODO: using sample conversaion for quick testing and to reduce Deepgram usage. change to use the real conversation
+        gptService.completion(sampleConversation, 10);
+      }
+
     }
   });
 
-  transcriptionService.on("utterance", async (text) => {
+  patient_transcriptionService.on("utterance", async (text) => {
     // This is a bit of a hack to filter out empty utterances
     if(marks.length > 0 && text?.length > 5) {
-      console.log("Twilio -> Interruption, Clearing stream".red)
+      console.log("Twilio -> Interruption, Clearing Patient stream".red)
       ws.send(
         JSON.stringify({
-          streamSid,
+          patientStreamSid,
           event: "clear",
         })
       );
     }
   });
 
-  transcriptionService.on("transcription", async (text) => {
+  clinician_transcriptionService.on("utterance", async (text) => {
+    // This is a bit of a hack to filter out empty utterances
+    if(marks.length > 0 && text?.length > 5) {
+      console.log("Twilio -> Interruption, Clearing Clinician stream".red)
+      ws.send(
+        JSON.stringify({
+          clinicianStreamSid,
+          event: "clear",
+        })
+      );
+    }
+  });
+
+
+  patient_transcriptionService.on("transcription", async (text) => {
     if (!text) { return; }
-    console.log(`Interaction ${interactionCount} – STT -> GPT: ${text}`.yellow);
-    gptService.completion(text, interactionCount);
+    conversation = conversation + "\r\n Patient: " + text;
+    console.log(`Patient interaction ${interactionCount} – STT -> GPT: ${text}`.yellow);
+    //gptService.completion(text, interactionCount);
+    interactionCount += 1;
+  });
+
+  clinician_transcriptionService.on("transcription", async (text) => {
+    if (!text) { return; }
+    conversation = conversation + "\r\n Clinician: " + text;
+    console.log(`Clinician interaction ${interactionCount} – STT -> GPT: ${text}`.yellow);
+    //gptService.completion(text, interactionCount);
     interactionCount += 1;
   });
   
   gptService.on('gptreply', async (gptReply, icount) => {
     console.log(`Interaction ${icount}: GPT -> TTS: ${gptReply.partialResponse}`.green )
-    ttsService.generate(gptReply, icount);
+    //ttsService.generate(gptReply, icount);
   });
 
   ttsService.on("speech", (responseIndex, audio, label, icount) => {
--- services/gpt-service.js
+++ services/gpt-service.js
@@ -15,9 +15,11 @@ class GptService extends EventEmitter {
   constructor() {
     super();
     this.openai = new OpenAI();
+    //TODO: tweak the system and assistant roles
     this.userContext = [
-      { "role": "system", "content": "You are an outbound sales representative selling Apple Airpods. You have a youthful and cheery personality. Keep your responses as brief as possible but make every attempt to keep the caller on the phone without being rude. Don't ask more than 1 question at a time. Don't make assumptions about what values to plug into functions. Ask for clarification if a user request is ambiguous. Speak out all prices to include the currency. Please help them decide between the airpods, airpods pro and airpods max by asking questions like 'Do you prefer headphones that go in your ear or over the ear?'. If they are trying to choose between the airpods and airpods pro try asking them if they need noise canceling. Once you know which model they would like ask them how many they would like to purchase and try to get them to place an order. You must add a '•' symbol every 5 to 10 words at natural pauses where your response can be split for text to speech." },
-      { "role": "assistant", "content": "Hello! I understand you're looking for a pair of AirPods, is that correct?" },
+     // { "role": "system", "content": "You are an outbound sales representative selling Apple Airpods. You have a youthful and cheery personality. Keep your responses as brief as possible but make every attempt to keep the caller on the phone without being rude. Don't ask more than 1 question at a time. Don't make assumptions about what values to plug into functions. Ask for clarification if a user request is ambiguous. Speak out all prices to include the currency. Please help them decide between the airpods, airpods pro and airpods max by asking questions like 'Do you prefer headphones that go in your ear or over the ear?'. If they are trying to choose between the airpods and airpods pro try asking them if they need noise canceling. Once you know which model they would like ask them how many they would like to purchase and try to get them to place an order. You must add a '•' symbol every 5 to 10 words at natural pauses where your response can be split for text to speech." },
+     { "role": "system", "content": "Summarize this clinician and patient conversaton as clinician is taking medical notes in 2 sentences. then list the action items" },
+     // { "role": "assistant", "content": "Hello! I understand you're looking for a pair of AirPods, is that correct?" },
     ],
     this.partialResponseIndex = 0
   }
